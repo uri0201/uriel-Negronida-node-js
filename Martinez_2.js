adfe7049165c50cd8819215efa81719ce7cfe095
uri0201 <135330195+uri0201@users.noreply.github.com>
2023-06-08 22:08:36 -0300

    ejercicio-2-node.js

---
+++ Martinez_2.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+var numero1= parseInt(prompt('Ingrese un número: '));
+var numero2= parseInt(prompt('Ingrese otro número: ')); 
+var resultado;
+resultado=numero1+numero2;
+console.log('La suma es: ', resultado);
